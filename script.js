11885d0d93e0fe25890445ba5d9ac3394b1e5551
Pavel Trofimov <webtrofimov@gmail.com>
2021-04-13 14:49:43 +0300

    Работа условий в JS(3 способа)

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-let money = prompt("Ваш бюджет на месяц?", ''),
-	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let num = 50;
 
-let appData = {
-	budget: money,
-	expenses: {},
-	optionalExpenses: {},
-	income: [],
-	timeData: time,
-	savings: false
-};
+if (num <49){
+	console.log('Неверно!')
+} else if (num >100){
+	console.log('Много!')
+}else {
+	console.log('Верно!')
+} /* 1-й способ написания кода сверки */
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
+(num ==50) ? console.log('Verno!') : console.log('Neverno!'); /* 2-ой способ написания сверки */
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
-
-alert(appData.budget / 30);
+switch (num){
+	case num <49:
+		console.log('Neverno');
+		break;
+	case num > 100:
+		console.log('Mnogo!');
+		break;
+	case num > 80:
+		console.log('Vse eshe mnogo!');
+		break;
+	case 50:
+		console.log('Verno!!');
+		break;
+}
